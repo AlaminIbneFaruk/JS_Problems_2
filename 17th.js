2bdbc027a9e7472dcf3cbbcf454fe8bfb42f56f2
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-10-05 20:03:48 +0600

    17th, 18th done

--- 17th.js
+++ 17th.js
@@ -1,1 +1,5 @@
 // Print each character of a string in a new line
+let str=prompt("Enter a string:");
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i],"\n");
+}
--- 18th.js
+++ 18th.js
@@ -1,1 +1,9 @@
 // Remove vowels from string
+let str =prompt("Enter a string: ");
+for(let i=0; i<str.length; i++){
+    if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U'){
+        str = str.replace(str[i], '');
+        i--;
+    }
+}
+console.log(str);
