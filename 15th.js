feee5a3c293903eee133e94fa33a0a272d570325
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-10-05 19:52:46 +0600

    15th, 16th done

--- 15th.js
+++ 15th.js
@@ -1,1 +1,9 @@
 // Count Spaces in a Sentence
+let sentence = prompt("Enter a sentence:");
+let spaces = 0;
+for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === ' ') {
+        spaces++;
+    }  
+}
+console.log("Number of spaces: " + spaces);
--- 16th.js
+++ 16th.js
@@ -1,1 +1,11 @@
 // Find character frequency in a string
+// Example: "banana" → a = 3, n = 2, b = 1
+let str = prompt("Enter a string:").toLocaleLowerCase();
+let charFreq=prompt("Enter a character to find its frequency:").toLocaleLowerCase();
+let count = 0;
+for (let i=0;i<=str.length;i++){
+    if(str[i]===charFreq){
+        count++;
+    }
+}
+console.log("Frequency of '" + charFreq + "': " + count);
