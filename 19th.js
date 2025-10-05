30eaa13e70d779306dd4daf8428400bbd0a1b1c1
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-10-05 20:20:11 +0600

    19th, 20th done

--- 19th.js
+++ 19th.js
@@ -1,1 +1,9 @@
 // word counter
+let str=prompt("Enter a string:");
+let count=1;
+for (let i=0;i<str.length;i++){
+    if (str[i]==" "){
+        count++;
+    }
+}
+console.log("Number of words: " + (count));
--- 20th.js
+++ 20th.js
@@ -1,1 +1,11 @@
 // capitalize the first letter of each word in a string
+let str=prompt("Enter a string:");
+let afterUpper="";
+let strArr=str.split(""); 
+for (let i=0;i<strArr.length;i++){
+    if (strArr[i-1]==" "|| i-1==-1){
+        strArr[i]=strArr[i].toLocaleUpperCase();
+    }
+    afterUpper+=strArr[i];
+}
+console.log(afterUpper);
