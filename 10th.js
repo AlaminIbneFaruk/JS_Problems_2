7473ba5ce3a045fb6b6619a6bb01af1363ee40fe
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-10-05 19:02:43 +0600

    9th, 10th done

--- 10th.js
+++ 10th.js
@@ -1,2 +1,7 @@
 // Multiples of 3
 // 1 to 50
+for (let i = 0; i < 50; i++) {
+    if (i%3 ===0){
+        console.log(i,"is a multiple of 3");
+    }
+}
--- 9th.js
+++ 9th.js
@@ -1,1 +1,8 @@
 // Sum of even nums 1 to n
+let n=prompt("Enter a number: ");
+let sum =0;
+for (let i = 0; i < n; i+=2) {
+    sum+=i;
+    console.log(i);
+}
+console.log("Sum of even numbers from 1 to "+n+" is: "+sum);
