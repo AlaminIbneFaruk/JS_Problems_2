4863938b2d66f3e6116ebe554e675b6e994dc89a
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-10-05 19:32:07 +0600

    13th, 14th done

--- 13th.js
+++ 13th.js
@@ -1,1 +1,7 @@
 // Reverse a string
+let str =prompt("Enter a string: ");
+let revStr="";
+for(let i=str.length-1;i>=0;i--){
+    revStr+=str[i];
+}
+console.log("Reversed string is: "+revStr);
--- 14th.js
+++ 14th.js
@@ -1,1 +1,11 @@
 // Palindrome checker
+let str =prompt("Enter a string: ");
+let revStr="";
+for(let i=str.length-1;i>=0;i--){
+    revStr+=str[i];
+}
+if(str===revStr){
+    console.log(str+" is a palindrome");
+}else{
+    console.log(str+" is not a palindrome");
+}
