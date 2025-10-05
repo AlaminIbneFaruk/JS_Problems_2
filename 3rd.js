c56a1197f07469a51ab0f3c5c9ad9fd852606a09
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-10-05 18:37:12 +0600

    3rd done

--- 3rd.js
+++ 3rd.js
@@ -1,1 +1,7 @@
 // print sum of 1 to n using for / while
+let n = prompt("Enter the number untill the total sum is going to happen:");
+let sum = 0;
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log(sum);
