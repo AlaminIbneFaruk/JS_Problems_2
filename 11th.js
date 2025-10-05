2f937f78a070997f0d9ec2ee6fa8ca3215feb8a8
alaminibnefarukayon <alaminibnefarukayon@gmail.com>
2025-10-05 19:27:13 +0600

    11th, 12th done

--- 11th.js
+++ 11th.js
@@ -1,1 +1,9 @@
 // string length without using str.length
+let str = prompt("Enter a string:");
+let count = 0;
+
+while (str.charAt(count) !== "") {
+  console.log(str.charAt(count));
+  count++;
+}
+console.log("Length of the string is: " + count);
--- 12th.js
+++ 12th.js
@@ -1,1 +1,19 @@
 // counting vowels and consonants
+let str = prompt("Enter a string:");
+let vowels = 0;
+let consonants = 0;
+for (let i = 0; i < str.length; i++) {
+  if (
+    str.charAt(i) === "a" ||
+    str.charAt(i) === "e" ||
+    str.charAt(i) === "i" ||
+    str.charAt(i) === "o" ||
+    str.charAt(i) === "u"
+  ) {
+    vowels++;
+  } else {
+    consonants++;
+  }
+}
+console.log("Vowels: " + vowels);
+console.log("consonants: " + consonants);
